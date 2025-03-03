1bb0badd43d1786ff25347b085f44e30b880242d
Lucia Paricio <lpg103@alu.ua.es>
2025-03-03 17:52:48 +0100

    48800713A Implemented class Barco

--- hada-p2/Barco.cs
+++ hada-p2/Barco.cs
@@ -4,9 +4,77 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace hada_p2
+namespace Hada
 {
-    internal class Barco
+    public class Barco
     {
+        public Dictionary<Coordenada, String> CoordenadasBarco { get; private set; }
+        public string Nombre { get; set; }
+        public int NumDanyos { get; set; }
+
+        //declaracion de los eventos
+        public event EventHandler<TocadoArgs> EventoTocado;
+        public event EventHandler<HundidoArgs> EventoHundido;
+
+        public Barco (string nombre, int longitud, char orientacion, Coordenada coordenadaInicio)
+        {
+            Nombre = nombre;
+            NumDanyos = 0;
+            CoordenadasBarco = new Dictionary<Coordenada, string>();
+
+            for (int i = 0; i < longitud; i++)
+            {
+                Coordenada coord;
+                if (orientacion == 'h')
+                {
+                    coord = new Coordenada(coordenadaInicio.Fila, coordenadaInicio.Columna + i);    
+                }
+                else
+                {
+                    coord = new Coordenada(coordenadaInicio.Fila + i, coordenadaInicio.Columna);
+                }
+                CoordenadasBarco[coord] = Nombre;
+            }
+        }
+
+        public void Disparo (Coordenada c)
+        {
+            if (CoordenadasBarco.ContainsKey(c))
+            {
+                CoordenadasBarco[c] += "_T";
+                NumDanyos++;
+                EventoTocado(this, new TocadoArgs(Nombre, c));
+
+                if (Hundido())
+                {
+                    EventoHundido(this, new HundidoArgs(Nombre));
+                }
+            }
+
+        }
+
+        public bool Hundido()
+        {
+            foreach (var hundido in CoordenadasBarco.Values)
+            {
+                if (!hundido.EndsWith("_T")) return false;
+            }
+            return true;
+        }
+
+        public override string ToString()
+        {
+            bool estado = Hundido();
+            string resultado = $"[{Nombre}] - DAÑOS: [{NumDanyos}] - HUNDIDO: [{estado}] -  COORDENADAS:";
+            foreach(var coord in CoordenadasBarco)
+            {
+                resultado += $" [({coord.Key.Fila}, {coord.Key.Columna}) :{Nombre}]";
+            }
+            resultado += $"\n";
+            
+            return resultado;
+
+        }
+
     }
 }
