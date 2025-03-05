a8875aca2b172e465a31dd76938cd274aa9d22a5
Lucia Paricio <lpg103@alu.ua.es>
2025-03-05 15:13:10 +0100

    48800713A Implemented Game class

--- hada-p2/Game.cs
+++ hada-p2/Game.cs
@@ -6,7 +6,51 @@ using System.Threading.Tasks;
 
 namespace Hada
 {
-    internal class Game
+    public class Game
     {
+        private bool finPartida;
+        private void GameLoop()
+        {
+            List<Barco> barcos = new List<Barco>
+            {
+                new Barco ("SANTA MARIA", 2, 'v', new Coordenada(2, 3)),
+                new Barco ("LA NIÑA", 3, 'h', new Coordenada(6, 1)),
+                new Barco ("lA PINTA", 1, 'v', new Coordenada(5, 5))
+            };
+            Tablero tablero = new Tablero(9, barcos); // esto hay que revisarlo
+            while (!finPartida)
+            {
+                Console.Clear();
+                Console.WriteLine(tablero.DibujarTablero());
+                Console.Write("Introduce las cordenadas en el siguiente formato 'fila, columna' o 's' para finalizar la partida: ");
+                string entrada = Console.ReadLine();
+                if (entrada.ToLower() == "s")
+                {
+                    cuandoEventoFinPartida(this, EventArgs.Empty);
+                    
+                }
+                string[] partes = entrada.Split(',');
+                if(partes.Length == 2 && int.TryParse(partes[0], out int fila) && 
+                    int.TryParse(partes[1], out int columna))
+                {
+                    Coordenada disparo = new Coordenada(fila, columna);
+                    tablero.Disparar(disparo);
+                }
+                else
+                {
+                    Console.WriteLine("Formato incorreto, use el formato 'fila, columna'. ");
+                }
+            }
+        }
+        public Game()
+        {
+            finPartida = false;
+            GameLoop();
+        }
+        private void cuandoEventoFinPartida(object sender, EventArgs e)
+        {
+            Console.WriteLine("PARTIDA FINALIZADA!!\n");
+            finPartida = true;
+        }
     }
 }
