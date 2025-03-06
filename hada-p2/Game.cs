129768c74904b734063a06e7e665869278cb9582
Lucia Paricio <lpg103@alu.ua.es>
2025-03-06 09:55:27 +0100

    48800713A Added some changes to Game class

--- hada-p2/Game.cs
+++ hada-p2/Game.cs
@@ -11,6 +11,7 @@ namespace Hada
         private bool finPartida;
         private void GameLoop()
         {
+
             List<Barco> barcos = new List<Barco>
             {
                 new Barco ("SANTA MARIA", 2, 'v', new Coordenada(2, 3)),
@@ -20,31 +21,37 @@ namespace Hada
             Tablero tablero = new Tablero(9, barcos); // esto hay que revisarlo
             while (!finPartida)
             {
+
                 Console.Clear();
-                Console.WriteLine(tablero.DibujarTablero());
-                Console.Write("Introduce las cordenadas en el siguiente formato 'fila, columna' o 's' para finalizar la partida: ");
+                Console.WriteLine(tablero.ToString());
+                Console.Write("Introduce la cordenada a la que disparar FILA,COLUMNA ('S' para Salir): ");
                 string entrada = Console.ReadLine();
                 if (entrada.ToLower() == "s")
                 {
                     cuandoEventoFinPartida(this, EventArgs.Empty);
                     
                 }
-                string[] partes = entrada.Split(',');
-                if(partes.Length == 2 && int.TryParse(partes[0], out int fila) && 
-                    int.TryParse(partes[1], out int columna))
-                {
-                    Coordenada disparo = new Coordenada(fila, columna);
-                    tablero.Disparar(disparo);
-                }
                 else
                 {
-                    Console.WriteLine("Formato incorreto, use el formato 'fila, columna'. ");
+                    string[] partes = entrada.Split(',');
+                    if (partes.Length == 2 && int.TryParse(partes[0], out int fila) &&
+                        int.TryParse(partes[1], out int columna))
+                    {
+                        Coordenada disparo = new Coordenada(fila, columna);
+                        tablero.Disparar(disparo);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Formato incorreto, use el formato FILA, COLUMNA. ");
+                    }
                 }
+                
             }
         }
         public Game()
         {
             finPartida = false;
+
             GameLoop();
         }
         private void cuandoEventoFinPartida(object sender, EventArgs e)
