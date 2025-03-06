991da3e925fa367041edc6d823e3b50ecc270881
Lucia Paricio <lpg103@alu.ua.es>
2025-03-06 17:20:48 +0100

    48800713A Corrected class Game

--- hada-p2/Game.cs
+++ hada-p2/Game.cs
@@ -18,34 +18,58 @@ namespace Hada
                 new Barco ("LA NIÑA", 3, 'h', new Coordenada(6, 1)),
                 new Barco ("lA PINTA", 1, 'v', new Coordenada(5, 5))
             };
-            Tablero tablero = new Tablero(9, barcos); // esto hay que revisarlo
+            Tablero tablero = new Tablero(9, barcos);
+            int barcosTotales = barcos.Count;
+            int barcosHundidos = 0;
+
             while (!finPartida)
             {
-
-                Console.Clear();
-                Console.WriteLine(tablero.ToString());
-                Console.Write("Introduce la cordenada a la que disparar FILA,COLUMNA ('S' para Salir): ");
-                string entrada = Console.ReadLine();
-                if (entrada.ToLower() == "s")
+                
+                foreach (var barco in barcos)
                 {
-                    cuandoEventoFinPartida(this, EventArgs.Empty);
-                    
-                }
-                else
-                {
-                    string[] partes = entrada.Split(',');
-                    if (partes.Length == 2 && int.TryParse(partes[0], out int fila) &&
-                        int.TryParse(partes[1], out int columna))
+                    if (barco.Hundido())
                     {
-                        Coordenada disparo = new Coordenada(fila, columna);
-                        tablero.Disparar(disparo);
-                    }
-                    else
-                    {
-                        Console.WriteLine("Formato incorreto, use el formato FILA, COLUMNA. ");
+                        barcosHundidos += 1;
                     }
                 }
                 
+                if(barcosTotales == barcosHundidos)
+                {
+                    cuandoEventoFinPartida(this, EventArgs.Empty);
+                }
+                
+                else {
+                    Console.WriteLine(tablero.ToString());
+                    Console.Write("Introduce la cordenada a la que disparar FILA,COLUMNA ('S' para Salir): ");
+                    string entrada = Console.ReadLine();
+                    if (entrada.ToLower() == "s")
+                    {
+                        cuandoEventoFinPartida(this, EventArgs.Empty);
+
+                    }
+
+
+
+                    else
+                    {
+                        Console.Clear();
+
+                        string[] partes = entrada.Split(',');
+                        if (partes.Length == 2 && int.TryParse(partes[0], out int fila) &&
+                            int.TryParse(partes[1], out int columna))
+                        {
+                            Coordenada disparo = new Coordenada(fila, columna);
+                            tablero.Disparar(disparo);
+                        }
+                        else
+                        {
+
+                            Console.WriteLine("Formato incorreto, use el formato FILA, COLUMNA. ");
+                        }
+                    }
+                }
+                barcosHundidos = 0;
+
             }
         }
         public Game()
