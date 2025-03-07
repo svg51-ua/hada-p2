daae1f010e2a592238738aefb848800dc505103b
Sofia Vasileva <svg51@alu.ua.es>
2025-03-07 21:39:45 +0100

    X7270786A añadidos metodos para generacion aleatoria de barcos

--- hada-p2/Game.cs
+++ hada-p2/Game.cs
@@ -12,6 +12,7 @@ namespace Hada
     /// </summary>
     public class Game
     {
+        private static readonly Random random = new Random();
         private bool finPartida;
 
         /// <summary>
@@ -20,16 +21,12 @@ namespace Hada
         /// </summary>
         private void GameLoop()
         {
-            // declaramos e inicializamos los barcos
-            List<Barco> barcos = new List<Barco>
-            {
-                new Barco ("SANTA MARIA", 2, 'v', new Coordenada(2, 3)),
-                new Barco ("LA NIÑA", 3, 'h', new Coordenada(6, 1)),
-                new Barco ("LA PINTA", 1, 'v', new Coordenada(5, 5))
-            };
-            // declaramos el tablero
-            Tablero tablero = new Tablero(9, barcos);
-            int barcosTotales = barcos.Count;
+            int tamTablero = random.Next(4,10);
+            
+
+            List<Barco> barcos = generarBarcos(tamTablero);
+
+            Tablero tablero = new Tablero(tamTablero, barcos);
             tablero.EventoFinPartida += cuandoEventoFinPartida;
 
             while (!finPartida)
@@ -58,12 +55,121 @@ namespace Hada
 
                         Console.WriteLine("Formato incorreto, use el formato FILA, COLUMNA. ");
                     }
-                }
-                
+                }                
 
             }
         }
 
+        /// <summary>
+        /// Genera una lista de barcos con posiciones, orientaciones y tamaños aleatorios, comprobando que
+        /// no se superpongan entre ellos y estén siempre en el tablero.
+        /// </summary>
+        /// <param name="cantidad">cantidad de barcos a generar</param>
+        /// <param name="tamTablero">tamaño del tablero</param>
+        /// <returns>Lista final con todos los barcos generados</returns>
+        public List<Barco> generarBarcos(int tamTablero)
+        {
+            List<string> nombres = new List<string> { "SANTA MARIA", "LA NIÑA", "LA PINTA", "LOKI", "THOR", "MAYA" };
+            List<Barco> barcos = new List<Barco>();
+            List<Coordenada> posicionesOcupadas = new List<Coordenada>();
+            int numBarcos = random.Next(3, 6);
+
+            for (int i = 0; i < numBarcos; i++)
+            {
+                // Elige un nombre de los disponibles, lo asigna y lo elimina de la lista para evitar duplicados
+                // en la próxima vez que se genere un barco
+                int randomNombre = random.Next(nombres.Count);
+                string nombre = nombres[randomNombre];
+                nombres.RemoveAt(randomNombre); 
+
+                int tamBarco = random.Next(1, 4);
+
+                // determina la orientación del barco
+                char orientacion;
+                if (random.Next(2) == 0)
+                {
+                    orientacion = 'h';
+                }else
+                {
+                    orientacion = 'v';
+                }
+
+                // genera una coordenada aleatoria y comprueba que este disponible
+                Coordenada pos;
+                do
+                {
+                    int fila = random.Next(0, tamTablero);
+                    int columna = random.Next(0, tamTablero);
+                    pos = new Coordenada(fila, columna);
+                } while (!comprobarCoordenada(pos, tamBarco, orientacion, tamTablero, posicionesOcupadas));
+
+                // Añade un barco a la lista y registra las posiciones que ocupa.
+                barcos.Add(new Barco(nombre, tamBarco, orientacion, pos));
+                for (int j = 0; j < tamBarco; j++)
+                {
+                    int fila, columna;
+
+                    if (orientacion == 'h')
+                    {
+                        fila = pos.Fila;
+                        columna = pos.Columna + j;
+                    } else
+                    {
+                        fila = pos.Fila + j;
+                        columna = pos.Columna;
+                    }
+                    posicionesOcupadas.Add(new Coordenada(fila, columna));
+                }
+
+            }
+
+            return barcos;
+        }
+
+        /// <summary>
+        /// Método que comprueba si la coordenada en la que se quiere colocar un barco está disponible.
+        /// Revisa si todas las casillas que ocuparía un barco están libres y dentro del tablero
+        /// </summary>
+        /// <param name="posicion">posicion inicial del barco</param>
+        /// <param name="tamañoBarco">numero de casillas que ocupa el barco</param>
+        /// <param name="orientacion">orientacion del barco</param>
+        /// <param name="tamTablero">tamaño del tablero</param>
+        /// <param name="ocupadas">lista con las posiciones uqe ya estan ocupadas</param>
+        /// <returns></returns>
+        private bool comprobarCoordenada(Coordenada posicion, int tamañoBarco, char orientacion, int tamTablero, List<Coordenada> ocupadas)
+        {
+            bool disponible = true;
+            for (int i = 0; i < tamañoBarco; i++)
+            {
+                int fila, columna;
+
+                if (orientacion == 'h')
+                {
+                    fila = posicion.Fila;
+                    columna = posicion.Columna + i;
+                } else
+                {
+                    fila = posicion.Fila + i;
+                    columna = posicion.Columna;
+                }
+
+                if(fila >=tamTablero || columna >= tamTablero)
+                {
+                    return false;
+                }
+
+                Coordenada nuevaPos = new Coordenada(fila, columna);
+
+                if (ocupadas.Contains(nuevaPos) || columna > tamTablero || fila > tamTablero)
+                {
+                    //disponible = false;
+                    return false;
+                }
+            }
+            return true;
+        }
+
+       
         /// <summary>
         /// Este metodo llama al bucle el juego y se utiliza para iniciar el juego.
         /// </summary>
